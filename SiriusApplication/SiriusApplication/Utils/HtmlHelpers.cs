e9e38123e05e7e99485abb6593dc1067b6605f49
BarryArnott <bazza.arnott@gmail.com>
2014-03-01 18:07:30 +0000

    fixed bug with navbar active class

--- SiriusApplication/SiriusApplication/Utils/HtmlHelpers.cs
+++ SiriusApplication/SiriusApplication/Utils/HtmlHelpers.cs
@@ -5,17 +5,18 @@ public static class HtmlHelpers
 {
     public static MvcHtmlString MenuLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName)
     {
+        //TODO add active class to hobbies & interests nav link
         var currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
         var currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
-
-        var builder = new TagBuilder("li")
-        {
-            InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToHtmlString()
-        };
+        var htmlWithClass = "";
+        var htmlWithNoClass = "<li><a href='/" + controllerName + "'>" + linkText + "</a></li>";
 
         if (controllerName == currentController && actionName == currentAction)
-            builder.AddCssClass("active");
+        {
+            htmlWithClass = htmlWithNoClass.Insert(3, " class='active'");
+            return new MvcHtmlString(htmlWithClass);
+        }
 
-        return new MvcHtmlString(builder.ToString());
+        return new MvcHtmlString(htmlWithNoClass);
     }
 }
