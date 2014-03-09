8cbf8453d4eb295aa3e23bd4fb410a315b9e9032
BarryArnott <test@test.com>
2014-03-09 13:04:09 +0000

    added chevrons to collapsible section

---
+++ SiriusApplication/SiriusApplication/Scripts/collapsibleChevrons.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    
+        $('#accordion .panel-collapse').on('shown.bs.collapse', function () {
+            $(this).prev().find(".glyphicon").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
+        });
+
+        //The reverse of the above on hidden event:
+        $('#accordion .panel-collapse').on('hidden.bs.collapse', function () {
+            $(this).prev().find(".glyphicon").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
+        });
+    });
