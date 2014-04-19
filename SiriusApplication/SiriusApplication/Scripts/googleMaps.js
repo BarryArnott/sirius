d7554221052b7eac69bae90de53c1a557c444b77
BarryArnott <test@test.com>
2014-04-19 17:26:57 +0100

    refactored googleMaps to remove hardcoded DOM element location

--- SiriusApplication/SiriusApplication/Scripts/googleMaps.js
+++ SiriusApplication/SiriusApplication/Scripts/googleMaps.js
@@ -1,31 +1,16 @@
-﻿// TODO: This is a horrible, hacky and static implementation. Complete refactor required to be more dynamic.
-
-// Initialise map when collapse location div is shown
-$('#collapseLocation0').on('shown.bs.collapse', function () {
-    var count = 0;
+﻿$('.panel-collapse').on('shown.bs.collapse', function (e) {
+    var increment = e.currentTarget.id.slice(-1);
+    var latitude = $('#latitude' + increment).attr("value");
+    var longitude = $('#longitude' + increment).attr("value");
     
-    GoogleMapInitialise(count);
-});
-
-// RInitialise map when collapse location div is shown
-$('#collapseLocation1').on('shown.bs.collapse', function () {
-    var count = 1;
-    
-    GoogleMapInitialise(count);
-});
-
-// Initialise map when collapse location div is shown
-$('#collapseLocation2').on('shown.bs.collapse', function () {
-    var count = 2;
-
-    GoogleMapInitialise(count);
+    GoogleMapInitialise(increment, latitude, longitude);
 });
  
-function GoogleMapInitialise(collapseLocation) {
+function GoogleMapInitialise(collapseLocation, latitude, longitude) {
 
     // Tell the api to use the new UI
     window.google.maps.visualRefresh = true;
-    var latitudeLongitude = new window.google.maps.LatLng(window.latitude, window.longitude);
+    var latitudeLongitude = new window.google.maps.LatLng(latitude, longitude);
 
     // These are options that set initial zoom level, where the map is centered globally to start, and the type of map to show
     var mapOptions = {
@@ -35,15 +20,15 @@ function GoogleMapInitialise(collapseLocation) {
     };
 
     // This makes the div with id "map_canvas" a google map
-
+    // TODO: Need to remove harcoding of 3 cases and refactor to be more dynamic
     switch (collapseLocation) {
-        case 0:
+        case '0':
             var map0 = new window.google.maps.Map(document.getElementById('map_canvas0'), mapOptions);
             break;
-        case 1:
+        case '1':
             var map1 = new window.google.maps.Map(document.getElementById('map_canvas1'), mapOptions);
             break;
-        case 2:
+        case '2':
             var map2 = new window.google.maps.Map(document.getElementById('map_canvas2'), mapOptions);
             break;
         default:
