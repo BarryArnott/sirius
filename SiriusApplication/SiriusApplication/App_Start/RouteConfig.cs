8d19387161a495c34b14bc95bf1e103cd15794ff
BarryArnott <test@test.com>
2014-05-03 17:37:10 +0100

    refactored image modal

--- SiriusApplication/SiriusApplication/App_Start/RouteConfig.cs
+++ SiriusApplication/SiriusApplication/App_Start/RouteConfig.cs
@@ -10,28 +10,6 @@ namespace SiriusApplication
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             AreaRegistration.RegisterAllAreas();
 
-            ////This route means we can access photos like this: /Photography/3
-            //routes.MapRoute(
-            //    name: "ImageIdRoute",
-            //    url: "Photography/{id}",
-            //    defaults: new { controller = "Photography", action = "DisplayImageById" },
-            //    constraints: new { id = "[0-9]+" }
-            //);
-
-            ////This route means we can access photos like this: /Photography/title/my%20photo%20title
-            //routes.MapRoute(
-            //    name: "ImageTitleRoute",
-            //    url: "Photography/title/{title}",
-            //    defaults: new { controller = "Image", action = "DisplayByTitle" }
-            //);
-
-            ////This route means we can access photos like this: /Photography/title/my%20photo%20title
-            //routes.MapRoute(
-            //    name: "ImageIndexRoute",
-            //    url: "Photography/",
-            //    defaults: new { controller = "Image", action = "Index" }
-            //);
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
