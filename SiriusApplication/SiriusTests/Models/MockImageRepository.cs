c082ecf2511a656f5e5bef09cb8eb7417376ae7d
BarryArnott <test@test.com>
2014-08-24 08:15:11 +0100

    Implementaion of mock repository for unit testing

---
+++ SiriusApplication/SiriusTests/Models/MockImageRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace SiriusTests.Models
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using SiriusApplication.Models;
+
+    class MockImageRepository : IImageRepository
+    {
+        private List<Image> _mockDB = new List<Image>(); 
+
+
+
+
+        public IQueryable<Image> Images { get; private set; }
+
+        public Image GetImageById(int id)
+        {
+            return _mockDB.FirstOrDefault(x => x.ImageId == id);
+        }
+
+
+
+
+
+
+
+
+        public Image GetImageByTitle(string title)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Image GetDefaultImageWhenNoImageFound()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Image> GetAllImagesOrderedDescending()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Image> GetImagesOrderedDescending(int number)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Image> GetImagesByAlbumId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
